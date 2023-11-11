161724321c06fb49cfb4e43815201b7984e29262
Nolan Luppke <nollupp@gmail.com>
2023-11-11 14:09:46 -0600

    Created simple web server

---
+++ web_server/target/debug/deps/web_server.d
@@ -0,0 +1,5 @@
+C:\Users\nollu\OneDrive\Desktop\Work\Learning Rust!\web_server\target\debug\deps\web_server.exe: src\main.rs
+
+C:\Users\nollu\OneDrive\Desktop\Work\Learning Rust!\web_server\target\debug\deps\web_server.d: src\main.rs
+
+src\main.rs:
---
+++ web_server/target/debug/web_server.d
@@ -0,0 +1,1 @@
+C:\Users\nollu\OneDrive\Desktop\Work\Learning\ Rust!\web_server\target\debug\web_server.exe: C:\Users\nollu\OneDrive\Desktop\Work\Learning\ Rust!\web_server\src\main.rs
